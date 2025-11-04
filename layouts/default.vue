82154f7a38ce71bf58a9086ffa4a8b505c599f58
NBBA772 <developer@businessbenefitalliance.com>
2025-11-04 13:57:39 -0500

    first commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,17 @@
 <script setup lang="ts">
 import Navbar from "~/components/layout/navbar.vue";
 import TheFooter from "~~/components/layout/TheFooter.vue";
+definePageMeta({
+  head: {
+    script: [
+      {
+        src: 'https://analytics.ahrefs.com/analytics.js',
+        'data-key': 'V0zZlhoarGMJAxDX81ncuA',
+        async: true
+      }
+    ]
+  }
+})
 </script>
 
 <template>
