218687628bd33956d2218e8ef873385002b6caa0
NBBA772 <developer@businessbenefitalliance.com>
2025-11-11 16:22:33 -0500

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -123,6 +123,13 @@ export default defineNuxtConfig({
       options: {
         target: 'esnext'
       }
+    },
+    routeRules: {
+      '/**': {
+        headers: {
+          'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google-analytics.com https://www.googletagmanager.com https://fonts.googleapis.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https: blob:; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https://www.google-analytics.com; frame-src 'self' https://www.google.com; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'self';"
+        }
+      }
     }
   },
     routeRules: {
